3e9b4ce1961e586d16624f478906a58d59720e45
allamymp <allamympereira@gmail.com>
2023-01-17 10:05:17 -0300

    fim aula abstracao e encapsulamento

--- OrangeTech/desafio-poo-dio/src/Main.java
+++ OrangeTech/desafio-poo-dio/src/Main.java
@@ -1,5 +1,38 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Locale.setDefault(Locale.US);
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descricao curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Js");
+        curso2.setDescricao("Descricao curso Js");
+        curso2.setCargaHoraria(2);
+
+
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descricacao mentoria de Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
+
+
     }
 }
